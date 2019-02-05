108598d387dac6cb346f1511805f0c4d9b390bdd
Andrii Nadezhdin <andrii.nadezhdin@betssongroup.com>
2019-02-05 15:26:50 +0200

    Add client password auth

--- IdentityServer/Client/Program.cs
+++ IdentityServer/Client/Program.cs
@@ -39,9 +39,35 @@ namespace Client
             Console.WriteLine(tokenResponse.Json);
             Console.WriteLine("\n\n");
 
-            // call api
+            await GetApiResponse(tokenResponse.AccessToken);
+
+            tokenResponse = await client.RequestPasswordTokenAsync(new PasswordTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+                ClientId = "ro.client",
+                ClientSecret = "secret",
+
+                UserName = "alice",
+                Password = "password",
+                Scope = "api1"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+            await GetApiResponse(tokenResponse.AccessToken);
+        }
+
+        private static async Task GetApiResponse(string accessToken)
+        {
             var apiClient = new HttpClient();
-            apiClient.SetBearerToken(tokenResponse.AccessToken);
+            apiClient.SetBearerToken(accessToken);
 
             var response = await apiClient.GetAsync("http://localhost:5001/identity");
             if (!response.IsSuccessStatusCode)
